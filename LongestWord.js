a959a7b95ddd3c5ec2ee237bb8be28f031a3c79e
vbdivya <46649262+vbdivya@users.noreply.github.com>
2019-01-13 21:06:56 +0530

    Create LongestWord.js

---
+++ LongestWord.js
@@ -0,0 +1,16 @@
+function LongestWord(sen)
+{
+  var array1 = sen.match(/\w[a-z]{0,}/gi);
+  var result = array1[0];
+
+  for(var x = 1 ; x < array1.length ; x++)
+  {
+    if(result.length < array1[x].length)
+    {
+    result = array1[x];
+    } 
+  }
+  return result;
+}
+
+console.log(LongestWord('Accionlabs Development company and Development center'));
