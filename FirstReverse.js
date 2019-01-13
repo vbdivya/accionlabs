3241b6a649e06f8b311172335d9625b5000f60d9
vbdivya <46649262+vbdivya@users.noreply.github.com>
2019-01-13 21:10:55 +0530

    Create FirstReverse.js

---
+++ FirstReverse.js
@@ -0,0 +1,9 @@
+function FirstReverse(str) {
+   var newString = "";
+   for (var i = str.length - 1; i >= 0; i--) {
+       newString += str[i];
+   }
+   return newString;
+}
+FirstReverse('hello Accionlabs');
+console.log(FirstReverse('hello Accionlabs'));
